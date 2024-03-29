c42dac5d63ab2162de6ce3f1cf0194c982d53ddb
Indrasen23 <126501057+Indrasen23@users.noreply.github.com>
2024-03-29 21:00:57 +0530

    test commit

--- Server.js
+++ Server.js
@@ -3,14 +3,47 @@ const mongoose = require('mongoose');
 const app = express()
 const db = require('./db')
 require('dotenv').config();
-
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./models/Person.js')
 
 const port = process.env.PORT_VAL || 3000
-
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json()); // req.body
 
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()} Request Made to : ${req.originalUrl}]`)
+    next(); // MOve on to the next phase (VERY IMP to call next func)
+}
+app.use(logRequest)
+
+
+passport.use(new LocalStrategy(async (USERNAME, PASSWORD, done) => {
+    // authentication logic here
+    try {
+        console.log('Received credentials', USERNAME, PASSWORD);
+        const user = await Person.findOne({ username: USERNAME });
+
+        if (!user) {
+            // done(error, user, info)
+            return done(null, false, { message: 'Incorrect username' })
+        }
+        const isPasswordMatch = (user.password === PASSWORD) ? true : false;
+        if (isPasswordMatch) {
+            return done(null, user);
+        } else {
+            return done(null, false, { message: 'Incorrect password.' })
+        }
+    }
+    catch (err) {
+        return done(err);
+    }
+}))
+
+app.use(passport.initialize());
 
 
 
--- db.js
+++ db.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 
 
 // URL for Local DB
-//    const mongoURL = process.env.MONGODB_URL_LOCAL // Find if there is db in the name of the hotels else create it.
+   const mongoURL = process.env.MONGODB_URL_LOCAL // Find if there is db in the name of the hotels else create it.
 
 
 
 // URL for Online DB (mongodb Atlas)
-const password = encodeURIComponent(process.env.DB_PASSWORD);
-const mongoURL = `mongodb+srv://admin:${password}@cluster0.rs6e4y9.mongodb.net`;
+// const password = encodeURIComponent(process.env.DB_PASSWORD);
+// const mongoURL = `mongodb+srv://admin:${password}@cluster0.rs6e4y9.mongodb.net`;
 
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
